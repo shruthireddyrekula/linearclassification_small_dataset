25b1ca1f90d953743f55c6c0f1c000d97364e5be
shruthi <shruthireddyrekula@gmail.com>
2023-05-04 15:25:39 -0400

    Create simple_classifier.py

---
+++ simple_classifier.py
@@ -0,0 +1,7 @@
+from sklearn import tree
+features = [[150,0],[170,0],[130,1],[120,1]]
+labels = [1,1,0,0]
+clf = Tree.desisionTree()
+clf = clf.fir(features, labels)
+prediction = clf.predict([150,0])
+print(prediction)
