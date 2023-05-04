c4fd47b4286b4ed0b24db99c77f72710c007b850
shruthi <shruthireddyrekula@gmail.com>
2023-05-04 15:45:08 -0400

    Update simple_classifier.py

--- simple_classifier.py
+++ simple_classifier.py
@@ -1,7 +1,7 @@
 from sklearn import tree
-features = [[150,0],[170,0],[130,1],[120,1]]
-labels = [1,1,0,0]
+features = [[150,0],[170,0],[130,1],[120,1]] #0 = bumpy 1 = smooth
+labels = [1,1,0,0] #1 = orange 0 =  apple
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-prediction = clf.predict([[150,1]])
+prediction = clf.predict([[150,0]])
 print(prediction)
