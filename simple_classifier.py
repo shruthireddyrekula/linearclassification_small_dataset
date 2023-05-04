3fbdef9873d90d827b436bead31a3768f3e16a3d
shruthi <shruthireddyrekula@gmail.com>
2023-05-04 15:38:35 -0400

    Update simple_classifier.py

--- simple_classifier.py
+++ simple_classifier.py
@@ -1,7 +1,7 @@
 from sklearn import tree
 features = [[150,0],[170,0],[130,1],[120,1]]
 labels = [1,1,0,0]
-clf = Tree.DecisionTreeClassifier()
+clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-prediction = clf.predict([150,0])
+prediction = clf.predict([[150,1]])
 print(prediction)
