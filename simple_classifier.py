6fb6d23fe733da6884dee26d21e3e59ec85f8774
shruthi <shruthireddyrekula@gmail.com>
2023-05-13 17:56:51 -0400

    Delete simple_classifier.py

--- simple_classifier.py
+++
@@ -1,6 +0,0 @@
-from sklearn import tree
-features = [[0.9],[1.0],[1.1],[1.2],[1.21],[0.1],[0.4],[0.2],[0.3],[0.45]]
-labels = [1,1,1,1,1,0,0,0,0,0]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features, labels)
-prediction = clf.predict([[0.51]])
